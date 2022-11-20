37d23d1d7b30d7fe470b98dab166683b21dfeb65
Tsygankov-Slava <st200399200399@gmail.com>
2022-11-20 18:29:55 +0300

    add .gitignore

--- cmake-build-debug/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tv/Desktop/task_1/googletest/googletest
-# Build directory: /Users/tv/Desktop/task_1/cmake-build-debug/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
